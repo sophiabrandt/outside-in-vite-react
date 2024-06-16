d82caabc76a2fa98b74fc5cadc68f7d692cacdc9
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2024-06-16 17:52:20 +0200

    feat: add Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
 /* v8 ignore start */
+import { ThemeProvider } from '@emotion/react';
 import { RestaurantStoreProvider } from './restaurants/RestaurantContext';
 import { RestaurantScreen } from './restaurants/RestaurantScreen';
+import {
+  AppBar,
+  Container,
+  CssBaseline,
+  Toolbar,
+  Typography,
+  createTheme,
+} from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff4400',
+    },
+    secondary: {
+      main: '#00ff44',
+    },
+  },
+});
 
 export const App = () => {
   return (
     <>
-      <RestaurantStoreProvider>
-        <RestaurantScreen />
-      </RestaurantStoreProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">Outside In Vite React</Typography>
+          </Toolbar>
+        </AppBar>
+        <Container>
+          <RestaurantStoreProvider>
+            <RestaurantScreen />
+          </RestaurantStoreProvider>
+        </Container>
+      </ThemeProvider>
     </>
   );
 };
